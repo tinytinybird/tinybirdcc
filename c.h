3a5362ec5e5e8722ecaa9d651992352824baced6
Dongpeng Xu <dongpengxu@psu.edu>
2015-07-13 16:06:35 -0400

    add c.h: common declaratons

---
+++ c.h
@@ -0,0 +1,23 @@
+/*
+ * Common Declarations
+ *
+ * - c.h is included by all modules.
+ * 
+ */
+
+/* Exported macros */
+#define NULL ((void*)0)
+#define NELEMS(a) ((int)(sizeof (a)/sizeof ((a)[0])))
+#define roundup(x,n) (((x)+((n)-1))&(~((n)-1)))
+
+/* Typedefs */
+
+/* #include "config.h" */
+
+/* Interface */
+
+/* Exported types */
+
+/* Exported data */
+
+/* Exported functions */
