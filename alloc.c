7113c49c1289856aa3ed0080e70223bfa500b0f7
Dongpeng Xu <dongpengxu@psu.edu>
2015-07-13 14:24:14 -0400

    add alloc.c: arena based memory allocation

---
+++ alloc.c
@@ -0,0 +1,84 @@
+/* 
+ * Arena based Memory Allocation
+ * 
+ */
+
+#include "c.h"
+
+/* Exported macros */
+#define NEW(p,a) ((p)) = allocate(sizeof *(p), (a))
+#define NEW0(p,a) memset(NEW((p),(a)), 0, sizeof *(p))
+
+/* Exported functions */
+extern void *allocate(unsigned long n, unsigned a);
+extern void deallocate(unsigned a);
+extern void *newarray(unsigned long m, unsigned long n, unsigned a);
+
+/* Types */
+struct block {
+    struct block *next;
+    char *limit;
+    char *avail;
+};
+
+union align {
+    long l;
+    char *p;
+    double d;
+    int (*f) (void);
+};
+
+union header {
+    struct block b;
+    union align a;
+};
+
+/* Data */
+static struct block
+    first[] = { { NULL }, { NULL }, { NULL } }, /* initiate with only one NULL? */
+    *arena[] = { &first[0], &first[1], &first[2] };
+
+static struct block *freeblocks;
+
+/* Functions */
+void *allocate(unsigned long n, unsigned a)
+{
+    struct block *ap;
+
+    ap = arena[a];
+    n = roundup(n, sizeof(union align));
+    while (ap->avail + n > ap->limit) {
+	if ((ap->next = freeblocks) != NULL) {
+	    freeblocks = freeblocks->next;
+	    ap = ap->next;
+	} else {
+	    unsigned m = sizeof (union header) + n + 10*1024;
+	    ap->next = malloc(m);
+	    ap = ap->next;
+	    if (ap == NULL) {
+		error("insufficient memory\n");
+		exit(1);
+	    }
+	    ap->limit = (char *)ap + m;	  /* limit is set up at the first time */
+	}                                 /* when the block is allocated */
+	/* set up avail and next for a block, new or from freeblocks */
+	ap->avail = (char *)((union header *)ap + 1);
+	ap->next = NULL;
+	arena[a] = ap;
+    }
+    ap->avail += n;
+    return ap->avail - n;
+}
+
+void *newarray(unsigned long m, unsigned long n, unsigned a)
+{
+    return allocate(m*n, a);
+}
+
+void deallocate(unsigned a) {
+    arena[a]->next = freeblocks;
+    freeblocks = first[a].next;
+    first[a].next = NULL;
+    arena[a] = &first[a];
+}
+
